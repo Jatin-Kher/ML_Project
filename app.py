dd669633615016aecf5b5b4026d392827f0b1ad3
Jatin Kher <jatinkher@Jatins-MacBook-Air.local>
2025-12-08 16:00:40 +0530

    data transformation

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 from src.MLproject.logger import logging 
 from src.MLproject.exception import CustomException
 from src.MLproject.components.data_ingestion import DataIngestion
-from src.MLproject.components.data_ingestion import DataIngestionConfig
+from src.MLproject.components.data_transformation import DataTransformationConfig, DataTransformation
 
 import sys
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     logging.info("the execution has statrted")
     
     try:
-        #data_ingestion_config=DataIngestionConfig()
+        # #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
-
-        # #data_transformation_config=DataTransformationConfig()
-        # data_transformation=DataTransformation()
-        # train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
-
-        # ## Model Training
-
-        # model_trainer=ModelTrainer()
-        # print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        
+        
+        # data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        
+        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        
+        
         
     except Exception as e:
         logging.info("dividing by zero is not allowed")
--- src/MLproject/components/data_ingestion.py
+++ src/MLproject/components/data_ingestion.py
@@ -13,35 +13,36 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts','train.csv')
-    test_data_path: str=os.path.join('artifacts','test.csv')
-    raw_data_path: str=os.path.join('artifacts','raw.csv')
-    
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
-        
+
     def initiate_data_ingestion(self):
         try:
-            df=read_sql_data()
-            logging.info("Reading from mySql databse")
-
+            ##reading the data from mysql
+            df=pd.read_csv(os.path.join('notebook/data','raw.csv'))
+            logging.info("Reading completed mysql database")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            train_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is completed")
+            logging.info("Data Ingestion is completed")
 
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+
+
             )
+
+
         except Exception as e:
             raise CustomException(e,sys)
